a5598cf27e5383949e7972ef4fa515b86700c642
Vitya <sexsportandrocknroll@gmail.com>
2020-05-01 23:37:36 +0300

    Basic fire

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -13,6 +13,8 @@ public class Hero : MonoBehaviour
 
     public float pitchMult = 30;
     public float gameRestartDelay = 2f;
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 40;
 
     [Header("Set Dynamically")] 
     [SerializeField]
@@ -58,6 +60,17 @@ public class Hero : MonoBehaviour
         transform.position = pos;
 
         transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            TempFire();
+        }
+    }
+
+    private void TempFire() {
+        GameObject projGO = Instantiate<GameObject>(projectilePrefab);
+        projGO.transform.position = transform.position;
+        Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+        rigidB.velocity = Vector3.up * projectileSpeed;
     }
 
     private void OnTriggerEnter(Collider other) {
