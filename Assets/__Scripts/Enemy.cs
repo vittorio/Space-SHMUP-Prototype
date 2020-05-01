dad4ea00342324b52407e069397589a25f98b74c
Vitya <sexsportandrocknroll@gmail.com>
2020-05-01 23:48:19 +0300

    Destroy enemy and projectile on collision

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -42,4 +42,15 @@ public class Enemy : MonoBehaviour
         tempPos.y -= speed * Time.deltaTime;
         pos = tempPos;
     }
+
+    private void OnCollisionEnter(Collision coll) {
+        GameObject otherGO = coll.gameObject;
+        if (otherGO.tag == "ProjectileHero") {
+            Destroy(otherGO);
+            Destroy(gameObject);
+        }
+        else {
+            print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+        }
+    }
 }
