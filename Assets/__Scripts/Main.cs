87715405e8ec548bea3284519309deaa0a53b317
Vitya <sexsportandrocknroll@gmail.com>
2020-05-07 23:21:32 +0300

    Basic weapon types config

--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -7,6 +7,7 @@ using Random = UnityEngine.Random;
 
 public class Main : MonoBehaviour {
     static public Main S;
+    static Dictionary<WeaponType, WeaponDefinition> WEAP_DICT;
 
     [Header("Set in Inspector")] 
     public GameObject[] prefabEnemies;
@@ -14,6 +15,8 @@ public class Main : MonoBehaviour {
     public float enemySpawnPerSecond = 0.5f;
 
     public float enemyDefaultPadding = 1.5f;
+
+    public WeaponDefinition[] weaponDefinitions;
 
     private BoundsCheck bndCheck;
 
@@ -23,6 +26,11 @@ public class Main : MonoBehaviour {
         bndCheck = GetComponent<BoundsCheck>();
         
         Invoke("SpawnEnemy", 1f/enemySpawnPerSecond);
+
+        WEAP_DICT = new Dictionary<WeaponType, WeaponDefinition>();
+        foreach (WeaponDefinition def in weaponDefinitions) {
+            WEAP_DICT[def.type] = def;
+        }
     }
 
     public void SpawnEnemy() {
@@ -49,5 +57,13 @@ public class Main : MonoBehaviour {
 
     private void Restart() {
         SceneManager.LoadScene("_Scene_0");
+    }
+
+    static public WeaponDefinition GetWeaponDefinition(WeaponType wt) {
+        if (WEAP_DICT.ContainsKey(wt)) {
+            return WEAP_DICT[wt];
+        }
+
+        return new WeaponDefinition();
     }
 }
